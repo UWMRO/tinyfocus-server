7c4bdc5bde577cab1ca65bdc12b5665ab4e7e8f9
Sebastian <sebcap@uw.edu>
2025-04-30 14:07:35 -0700

    add pip install

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Itty bitty focusing server (:
 
 This server will act as an in-between for the arduino and evora to move the focuser at the MRO.
 It also includes a mock arduino so that code can be tested without connected to the real arduino.
+
+Use `pip install .` to install required pip packages.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tinyfoucs",
+    version="1.0.0",
+    description="A tiny focus server for controlling a stepper motor.",
+    author="Astronomy Undergraduate Engineering Group",
+    install_requires=['flask[async]'],
+    packages=find_packages(exclude=["tests"]),
+)
