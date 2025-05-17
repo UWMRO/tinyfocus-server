b7becf43272c845985707ae82064dff78f5452da
ojf <ofraser@gmail.com>
2025-05-17 09:18:10 -0700

    Update Flask version
    Co-authored-by: Sebastian <115756919+seb-cap@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="1.0.0",
     description="A tiny focus server for controlling a stepper motor.",
     author="Astronomy Undergraduate Engineering Group",
-    install_requires=['flask[async], aiohttp'],
+    install_requires=['flask[async]>=2', 'aiohttp'],
     packages=find_packages(exclude=["tests"]),
 )
